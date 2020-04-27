cbb97bde101bd16a7fd35a8d37248b777b04a380
Vivian Li <vml39@cornell.edu>
2020-04-26 17:14:58 -0700

    add version 1 back

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,6 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.models.results import get_results
+from app.irsystem.models.results_version1 import get_results_version1
 
 project_name = "Karma Farmer"
 net_id = "Brian Lu: bl694, Maria Silaban: djs488, Vivian Li: vml39, William Wang: wow7, Yuna Shin: ys457"
@@ -12,6 +13,8 @@ def search():
 	query = query if query else ""
 	weight = request.args.get('weight')
 	weight = weight if weight else 50
+	version = int(request.args.get('version'))
+	version = version if version else 2
 	if not query:
 		data = []
 		count = 0
@@ -20,7 +23,8 @@ def search():
 		output_message = "Suggested Subreddits for: " + query
 		data = []
 		count = 0; 
-		for i in get_results(query, weight):
+		results = get_results(query, weight) if version == 2 else get_results_version1(query)
+		for i in results:
 			#data.append("subreddit: " + i['subreddit'] + ",   " + "score: " + str(i['score']))
 			data.append(i['subreddit'])
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, query=query, weight=weight)
